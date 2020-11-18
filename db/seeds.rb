bb43eef3f4192cd9f282db8064ae5e28327c2006
Coralie Delhommeau <coraliedelhommeau@gmail.com>
2020-11-18 14:37:43 +0100

    app done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating 5 restaurants"
+
+mc_do = Restaurant.create(name: 'Mc Do', address: 'Nantes', category: 'fast-food')
+kfc = Restaurant.create(name: 'KFC', address: 'Cholet', category: 'fast-food')
+petit_françois = Restaurant.create(name: 'Petit François', address: 'Nantes', category: 'French')
+chez_michel = Restaurant.create(name: 'Chez Michel', address: 'Lyon', category: 'French')
+guissepe = Restaurant.create(name: 'Guissepe', address: 'Bordeaux', category: 'Italian')
+
+puts "Finished!"
